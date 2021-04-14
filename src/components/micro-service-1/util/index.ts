295b904a152e2eaebc1ed98b476c37f9704f69cd
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-04-14 15:00:53 -0500

    export all

---
+++ src/components/micro-service-1/util/index.ts
@@ -0,0 +1,6 @@
+export * from "./createSet";
+export * from "./createThumbnail";
+export * from "./fileToBase64";
+export * from "./rotateImage";
+export * from "./storeImage";
+export * from "./types";
