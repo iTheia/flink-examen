c5c9d136bf77f2880d59bc8867df9f599ff44236
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-04-14 00:40:14 -0500

    problema 3

---
+++ src/components/problem-3/matchBrakets.ts
@@ -0,0 +1,21 @@
+const map = {
+  "(": ")",
+  "[": "]",
+  "{": "}",
+};
+
+export const matchBrakets = (string: string) => {
+  const stack = [];
+  for (let i = 0; i < string.length; i++) {
+    if (Object.keys(map).includes(string[i])) {
+      stack.push(string[i]);
+    } else {
+      if (!Object.values(map).includes(string[i])) {
+        continue;
+      }
+      const last = stack.pop();
+      if (string[i] !== map[last]) return false;
+    }
+  }
+  return stack.length === 0;
+};
