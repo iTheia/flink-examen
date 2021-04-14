d5da8b3696177fd8e12befc2e9cbe653556f3e61
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-04-14 16:54:08 -0500

    add socket io functions

--- src/http/http.ts
+++ src/http/http.ts
@@ -1,10 +1,12 @@
 import http from "http";
 import { Application } from "express";
+import { Server as SocketIOServer } from "socket.io";
 
 export class HTTPServer {
   public app: Application;
   public server: http.Server;
-
+  public io: SocketIOServer;
+  private ioHandlers = [];
   constructor(app: Application) {
     this.app = app;
     this.server = http.createServer(app);
@@ -12,5 +14,19 @@ export class HTTPServer {
 
   async start(port: number) {
     this.server.listen(port);
+  }
+
+  addSocketHandler(handler: Function) {
+    this.ioHandlers.push(handler);
+  }
+
+  createSocketIo() {
+    const io: SocketIOServer = require("socket.io")(this.server);
+    this.app.set("io", io);
+    io.on("connection", (socket) => {
+      this.ioHandlers.forEach((handler) => {
+        handler(socket, io);
+      });
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { sendData } from "./components";
 import { config } from "./config";
 import { app, HTTPServer } from "./http";
 
 export const server = new HTTPServer(app);
 
+server.createSocketIo();
+server.addSocketHandler(sendData);
 server.start(config.port);
