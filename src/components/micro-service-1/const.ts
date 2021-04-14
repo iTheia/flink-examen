4c69444861b547e5fe0516afdfb9e81cad4e5e72
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-04-14 15:01:13 -0500

    static paths

---
+++ src/components/micro-service-1/const.ts
@@ -0,0 +1,4 @@
+export const DOGS_ENDPOINT =
+  "https://api.thedogapi.com/v1/images/search?limit=3&mime_types=jpg";
+export const CATS_ENDPOINT =
+  "https://api.thecatapi.com/v1/images/search?limit=3&mime_types=jpg";
