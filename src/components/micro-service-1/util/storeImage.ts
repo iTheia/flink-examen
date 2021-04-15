39159d106637a6d9e34ca235b845c77e524bf8d4
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-04-14 23:18:44 -0500

    regresando index.html en dev

--- src/components/micro-service-1/util/storeImage.ts
+++ src/components/micro-service-1/util/storeImage.ts
@@ -5,14 +5,14 @@ import { v4 as uuid } from "uuid";
 import { config } from "../../../config";
 
 export const storeImage = (buffer: Buffer) => {
-  let path = join(server.app.get("publicPath"), "/images/", `${uuid()}.jpg`);
+  let path = join(server.app.get("publicPath"), "images/", `${uuid()}.jpg`);
   writeFileSync(path, buffer);
   path = path
     .replace(
       server.app.get("publicPath"),
       process.env.NODE_ENV === "production"
         ? `${config.host}`
-        : `${config.host}:${config.port}`
+        : `${config.host}:${config.port}/`
     )
     .replace(/\\/g, "/");
   return path
