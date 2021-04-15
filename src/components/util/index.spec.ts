225bf9010e5f73bb8770f54a5884c76a077e9ee2
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-04-14 19:46:02 -0500

    test random number gen

---
+++ src/components/util/index.spec.ts
@@ -0,0 +1,17 @@
+import { should, expect } from "chai";
+import { genRandomNumber } from "./index";
+import "jasmine";
+
+describe("Verify random number generator", () => {
+  it("generate a number betwen 1 and 10", () => {
+    const result = genRandomNumber();
+    expect(result).to.be.an("number").to.be.below(11).to.be.above(0);
+  });
+
+  it("throw 1 if not passing a number", () => {
+    // @ts-ignore
+    expect(genRandomNumber(10, false)).to.be.equal(1);
+    // @ts-ignore
+    expect(genRandomNumber(false)).to.be.equal(1);
+  });
+});
--- src/components/util/index.ts
+++ src/components/util/index.ts
@@ -1,2 +1,5 @@
-export const genRandomNumber = (max = 10, min = 1) =>
-  Math.floor(Math.random() * max) + min;
+export const genRandomNumber = (max = 10, min = 1) => {
+  if (typeof max !== "number") return 1;
+  if (typeof min !== "number") return 1;
+  return Math.floor(Math.random() * max) + min;
+};
