14eef593ebba520549891d594599b616ebf1917b
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-04-14 23:11:40 -0500

    less dogs and cats

--- src/components/micro-service-1/const.ts
+++ src/components/micro-service-1/const.ts
@@ -1,4 +1,4 @@
 export const DOGS_ENDPOINT =
-  "https://api.thedogapi.com/v1/images/search?limit=3&mime_types=jpg";
+  "https://api.thedogapi.com/v1/images/search?limit=2&mime_types=jpg";
 export const CATS_ENDPOINT =
-  "https://api.thecatapi.com/v1/images/search?limit=3&mime_types=jpg";
+  "https://api.thecatapi.com/v1/images/search?limit=1&mime_types=jpg";
